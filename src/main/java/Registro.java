9b34bc639171e4238dc967071840ef0bf81bea19
00jesus <j.acevedo03@ufromail.cl>
2023-04-20 16:05:13 -0400

    Se suben test de los metodos de mostrar cantidades funcionando

--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -5,46 +5,64 @@ public class Registro {
 
     }
 
-    public static int stringToInt (String edad) {
+    public static int stringToInt(String edad) {
         int resultado = Integer.parseInt(edad);
         return resultado;
     }
-    public static int mostrarCantidadMayores (String[][] personas) {
+
+    public static int mostrarCantidadMayores(String[][] personas) {
         int cantidad = 0;
-        int aux;
+        String aux;
+        int temp;
         for (int pers = 0; pers < 50; pers++) {
-                aux = stringToInt(personas[pers][2]);  //EDAD siempre esta en 3ra columna, por lo tanto, indice 2
-                if (aux >= 18 ) {
+            aux = personas[pers][2];//EDAD siempre esta en 3ra columna, por lo tanto, indice 2
+            if (aux == null) {
+                System.out.println("");
+            } else {
+                temp = stringToInt(aux);
+                if (temp >= 18 && temp < 65) {
                     cantidad = cantidad + 1;
                 }
-        }
-        return cantidad;
-}
-
-    public static int mostrarCantidadMenores (String [][] personas) {
-        int cantidad = 0;
-        int aux;
-        for (int pers = 0; pers < 50; pers++) {
-            aux = stringToInt(personas[pers][2]);  //EDAD siempre esta en 3ra columna, por lo tanto, indice 2
-            if (aux < 18 ) {
-                cantidad = cantidad + 1;
-            }
-        }
-        return cantidad;
-
-}
-    public static int mostrarCantidadTerceraEdad (String [][] personas) {
-        int cantidad = 0;
-        int aux;
-        for (int pers = 0; pers < 50; pers++) {
-            aux = stringToInt(personas[pers][2]);  //EDAD siempre esta en 3ra columna, por lo tanto, indice 2
-            if (aux >= 65  ) {
-                cantidad = cantidad + 1;
             }
         }
         return cantidad;
     }
 
+    public static int mostrarCantidadMenores(String[][] personas) {
+        int cantidad = 0;
+        String aux;
+        int temp;
+        for (int pers = 0; pers < 50; pers++) {
+            aux = personas[pers][2];//EDAD siempre esta en 3ra columna, por lo tanto, indice 2
+            if (aux == null) {
+                System.out.println("");
+            } else {
+                temp = stringToInt(aux);
+                if (temp < 18) {
+                    cantidad = cantidad + 1;
+                }
+            }
+        }
+        return cantidad;
+    }
+
+    public static int mostrarCantidadTerceraEdad(String[][] personas) {
+        int cantidad = 0;
+        String aux;
+        int temp;
+        for (int pers = 0; pers < 50; pers++) {
+            aux = personas[pers][2];//EDAD siempre esta en 3ra columna, por lo tanto, indice 2
+            if (aux == null) {
+                System.out.println("");
+            } else {
+                temp = stringToInt(aux);
+                if (temp >= 65) {
+                    cantidad = cantidad + 1;
+                }
+            }
+        }
+        return cantidad;
+    }
 }
 
 
--- src/test/java/RegistroTest.java
+++ src/test/java/RegistroTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class RegistroTest {
 
@@ -24,13 +24,38 @@ class RegistroTest {
 
     @Test
     void mostrarCantidadMayores() {
+        String [][] registro = new String[50][3];
+        String aux;
+        for (int i = 0; i < 3; i++) {
+            registro[i][2] = "25";
+        }
+        int esperado = 3;
+        int resultado = Registro.mostrarCantidadMayores(registro);
+        System.out.println(resultado);
+        Assertions.assertEquals(esperado, resultado);
     }
 
     @Test
     void mostrarCantidadMenores() {
+        String [][] registro = new String[50][3];
+        String aux;
+        for (int i = 0; i < 5; i++) { //son 5 menores de 15años
+            registro[i][2] = "15";
+        }
+        int esperado = 5;
+        int resultado = Registro.mostrarCantidadMenores(registro);
+        Assertions.assertEquals(esperado, resultado);
     }
 
     @Test
     void mostrarCantidadTerceraEdad() {
+        String [][] registro = new String[50][3];
+        String aux;
+        for (int i = 0; i < 17; i++) { //son 17 tercera edad de 77años
+            registro[i][2] = "77";
+        }
+        int esperado = 17;
+        int resultado = Registro.mostrarCantidadTerceraEdad(registro);
+        Assertions.assertEquals(esperado, resultado);
     }
 }
