daae7b1ce44fca132ec072ae81029cd652113210
00jesus <j.acevedo03@ufromail.cl>
2023-04-20 15:02:44 -0400

    Update de metodos a desarrollar, mostrar cantidades

--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -1,5 +1,25 @@
 public class Registro {
     public static void main(String[] args) {
+        String[][] registro = new String[50][3];
+
 
     }
+
+
+public static void mostrarCantidadMayores (String[][] personas) {
+
 }
+public static void mostrarCantidadMenores (String [][] personas) {
+
+}
+public static void mostrarCantidadTerceraEdad (String [][] personas) {
+
+}
+
+}
+
+/*
+    Queda pendiente como tratar:
+5) Mostrar la cantidad de personas según estado civil (Soltero/a - Casado/a).
+
+ */
