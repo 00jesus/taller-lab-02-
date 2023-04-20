ae203582893c9c63e23d77217686cf48a81b05c5
MCCGonzalez <m.gonzalez60@ufromail.cl>
2023-04-20 15:09:56 -0400

    Commit matias: se agrega el metodo menu y de salir

--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -1,5 +1,25 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Registro {
     public static void main(String[] args) {
 
+
+    }
+    public static void menu(){
+
+        System.out.println("Bienvenido, ingrese un numero"
+                + "\n1) Agregar persona:"
+                + "\n2) Mostrar personas mayores de edad"
+                + "\n3) Mostrar personas menores de edad"
+                + "\n4) Mostrar cantidad de personas de tercera dedad"
+                + "\n5) Mostar cantidad de personas segun estado civil"
+                + "\n6) salir");
+    }
+    public static String teclado(){
+        return new Scanner(System.in).nextLine();
+    }
+    private static void salir(){
+        System.exit(0);
     }
 }
